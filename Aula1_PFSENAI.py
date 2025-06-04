4ec495116259967f4e1f933d6bace17f29966f0c
Thiago S Oliveira <thiagosdeoliveira@gmail.com>
2025-06-03 22:18:42 -0300

    Update Aula1_PFSENAI.py

--- Aula1_PFSENAI.py
+++ Aula1_PFSENAI.py
@@ -1,1 +1,14 @@
+# Função print mostra o texto na tela!
+print("Olá Mundo!!!")
 
+# Colocando 3x ''' ele vira comentario para varias linhas
+
+# Entrada de Dados
+nome = input("Digite seu nome")
+print("Olá",nome)
+
+''' Tipos de dados básico
+int = número inteiro (Exemplo: 1, 23 , 56)
+float = número deimal (Exemplo: 3.14)
+str = texto (Exemplo: Python, Thiago)
+bool = lógica (verdadeiro/falso - true/false)'''
